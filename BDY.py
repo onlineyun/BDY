8cd96bb0ab82501219a11be628b768f6c18cfbcb
流浪网路 <32214255+onlineyun@users.noreply.github.com>
2019-03-28 16:08:00 +0800

    更新re匹配不全的影响

--- BDY.py
+++ BDY.py
@@ -2,7 +2,7 @@ import requests
 import json
 import re
 
-def get_content(url,data):
+def get_content(url, data):
     headers = {
         'Connection': 'keep-alive',
         # 'Content-Length': '113',
@@ -14,18 +14,20 @@ def get_content(url,data):
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7'
     }
 
-    response = requests.post(url,headers=headers,data=data)
+    response = requests.post(url, headers = headers, data = data)
     return response.json()
 def search(sea_url):
 
-    uuids = matchall(sea_url, ['pan.baidu.com/s/1([\w]+)'])
+    uuids = matchall(sea_url, ['pan.baidu.com/s/1([\w-]+)'])
+    print(uuids)
 
     if  uuids:
         data = {'uuids':'BDY-{}'.format(uuids[0]),
                'client_version':'2018.12'}
+			   
         req_url = 'https://ypsuperkey.meek.com.cn/api/v1/item/check-data'
 
-        html = get_content(req_url,data)
+        html = get_content(req_url, data)
         print(html)
     else:
         print("查询的url匹配不正确")
@@ -54,8 +56,9 @@ def matchall(text, patterns):
 
     return None
 if __name__ == '__main__':
-    '''
+    '''链接必须包含
     pan.baidu.com/s/1    
     '''
-    url = 'pan.baidu.com/s/191xssKElUKt0d6zz0TgqpA'
+	
+    url = 'pan.baidu.com/s/1ywx4EEp_sR2AhYoH-7pYuA'
     search(url)
